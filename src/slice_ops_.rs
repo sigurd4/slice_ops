6ef0c5827a9fb73de94ab3c0d1556b07b76843b5
Sigurd Sætherø Spangelo <sigurd-s@online.no>
2024-01-23 18:34:04 +0100

    Add doctest

--- src/slice_ops_.rs
+++ src/slice_ops_.rs
@@ -187,21 +187,43 @@ pub trait SliceOps<T>: Slice<Item = T>
     fn spread_ref<const M: usize>(&self) -> [&[Padded<T, M>]; M]
     where
         [(); M - 1]:;
+    
+    /// Spreads elements equally across `M` mutable slices.
+    /// Slices will have equal length only if the operand slice's length is divisible by `M`.
+    /// 
+    /// # Examples
+    /// ```rust
+    /// #![feature(generic_const_exprs)]
+    /// 
+    /// use slice_ops::SliceOps;
+    /// 
+    /// let mut arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
+    /// let slice = arr.as_mut_slice();
+    /// 
+    /// let [_, _, fizz] = slice.spread_mut();
+    /// assert_eq!(fizz, ["3", "6", "9", "12", "15"]);
+    /// for fizz in fizz.iter_mut()
+    /// {
+    ///     **fizz = "fizz";
+    /// }
+    /// 
+    /// let [_, _, _, _, buzz] = slice.spread_mut();
+    /// assert_eq!(buzz, ["5", "10", "fizz"]);
+    /// for buzz in buzz.iter_mut()
+    /// {
+    ///     if **buzz == "fizz"
+    ///     {
+    ///         **buzz = "fizzbuzz";
+    ///         continue;
+    ///     }
+    ///     **buzz = "buzz";
+    /// }
+    /// 
+    /// assert_eq!(arr, ["1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizzbuzz"]);
+    /// ```
     fn spread_mut<const M: usize>(&mut self) -> [&mut [Padded<T, M>]; M]
     where
         [(); M - 1]:;
-}
-
-#[test]
-fn test()
-{
-    let arr = [1, 2, 3];
-    let slice = arr.as_slice();
-
-    let [a, b] = slice.spread_ref();
-
-    assert_eq!(a, [1, 3]);
-    assert_eq!(b, [2]);
 }
 
 impl<T> const SliceOps<T> for [T]
